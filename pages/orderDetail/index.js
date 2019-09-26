e4c6b282ade14d17768b7a4775b496d3febb7765
wangchuan <wangzefu520@sohu.com>
2019-09-27 01:48:15 +0800

    解决优惠价格算错问题

--- pages/orderDetail/index.js
+++ pages/orderDetail/index.js
@@ -9,9 +9,13 @@ let baseObj = {
 
     if(options.params){
       let query = JSON.parse(options.params)
+      let profitAmount = query.totalAmount - query.amount;
+      profitAmount = profitAmount.toFixed(2);
+      console.log(query);
       this.createCanvas(query.no)
       this.getBanner()
       this.setData({
+        profitAmount: profitAmount,
         params:query
       })
     }
